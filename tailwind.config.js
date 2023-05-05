804d158412fba018ca3f9e522c800ba26b369a0d
kauankarvalho <kauan.karvalho@outlook.com>
2023-05-05 15:17:38 -0300

    feat: add background, header with logo, navigation, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      jetbrains_mono: ["JetBrains Mono", "monospace"],
+      open_sans: ["Open Sans", "sans-serif"],
+    },
+  },
   plugins: [],
 }
